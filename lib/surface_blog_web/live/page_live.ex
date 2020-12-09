7280725a39e0b8b75cf1783bbb317c469336b562
arnaud cormier <arno.cormier@gmail.com>
2020-12-09 17:42:58 +0100

    add sface page_live template and remove render from page_live

--- lib/surface_blog_web/live/page_live.ex
+++ lib/surface_blog_web/live/page_live.ex
@@ -2,10 +2,4 @@ defmodule SurfaceBlogWeb.PageLive do
    use Surface.LiveView
    alias SurfaceBlogWeb.Components.Button
 
-  def render(assigns) do
-    ~H"""
-    <h1>a default Button component</h1>
-    <Button />
-    """
-  end
 end
